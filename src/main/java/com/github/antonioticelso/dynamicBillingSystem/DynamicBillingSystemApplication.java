480f68e7660f59c82e0567459556a9e506c165c8
Antonio <antonio.ti.celso@gmail.com>
2023-10-22 11:14:38 -0300

    First commit

---
+++ src/main/java/com/github/antonioticelso/dynamicBillingSystem/DynamicBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.dynamicBillingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicBillingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/antonioticelso/dynamicBillingSystem/DynamicBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.dynamicBillingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
